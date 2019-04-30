9bd0a3363702b94047181e8e035f720070da5585
murad <muradkv@gmail.com>
2019-04-30 23:20:00 +0300

    Add UI: label price, imageView, currencyPicker

--- Bitcoin Ticker/ViewController.swift
+++ Bitcoin Ticker/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
